5ec1fd27b4c040c2225c2bbf97b4e52996d90bed
bhc1010 <61128244+bhc1010@users.noreply.github.com>
2020-03-10 00:20:37 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import network2
 import polytope
 import dataLoader
 import numpy as np
 
-imbedDim = 5
+imbedDim = 25
+
 
 poly0 = polytope.Polytope([(0.,1.),(-1.,-1.), (1.,0.)], 0, imbedDim)
 poly1 = polytope.Polytope([(-1.,2.),(-1.,-1.),(2.,-1.)], 1, imbedDim)
@@ -28,56 +28,26 @@ poly7 = polytope.Polytope([(0.,1.),(-1.,1.),(-1.,-1.),(1.,-1.),(1.,0.)], 7, imbe
 # poly15 = polytope.Polytope([(0.,1.),(-1.,1.),(-1.,-1.),(1.,-1.),(1.,0.)], 7, imbedDim)
 
 polytopes = [poly0, poly1, poly2, poly3]
-# polytopes = [poly0, poly1]
 
 for poly in polytopes:
     poly.generateData()
-    for i in range(len(poly.translations)):
-        print(len(poly.translations[i]))
 
 ## Note batch_size must divide evenly into len(train_data_raw)
 train_batches, train_labels, test_batches, test_labels = dataLoader.loadData(polytopes, batch_size=12)
 
-net = network2.Net([2 * imbedDim, 14, 8])
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("Running on the gpu")
+else:
+    device = torch.device("cpu")
+    print("Running on the cpu")
+    
+# device = torch.device("cpu")
+net = Net([2 * imbedDim, 25,25,25, 8]).to(device)
+net.train(10)
+accuracy = net.test()
 
-count = 0
-
-for lr, EPOCHS in zip([0.001],[1000]):
-    print("---------- Learning Rate: {} ----------".format(lr))
-    optimizer = optim.Adam(net.parameters(), lr=lr)
-    # EPOCHS = 500
-    for epoch in range(EPOCHS):
-        for X, Y in zip(train_batches, train_labels):
-            net.zero_grad()
-            output = net(X.view(-1,2*imbedDim))
-            loss = F.nll_loss(output, Y)
-            loss.backward()
-            optimizer.step()
-        count += 1
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for X, Y in zip(test_batches, test_labels):
-                output = net(X.view(-1,2*imbedDim))
-                for idx, i in enumerate(output):
-                    if torch.argmax(i) == Y[idx]:
-                        correct += 1
-                    total += 1
-        print("Epoch {} / {} | Loss: {} | Accuracy: {}".format(count, 100, np.round(loss.detach().numpy(), 4), round(correct*100 / total,2)))
-
-correct = 0
-total = 0
-
-with torch.no_grad():
-    for X, Y in zip(test_batches, test_labels):
-        output = net(X.view(-1,2*imbedDim))
-        for idx, i in enumerate(output):
-            if torch.argmax(i) == Y[idx]:
-                correct += 1
-            total += 1
-print("Accuracy: ", correct / total)
-
-if (correct*100 / total) >= 80:
+if accuracy * 100 >= 80:
     torch.save(net.state_dict(),'model4.pt')
 
 
