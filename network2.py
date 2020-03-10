364160075d78a370046c87928f23830280151e6a
bhc1010 <61128244+bhc1010@users.noreply.github.com>
2020-03-10 00:21:09 -0300

    Update network2.py

--- network2.py
+++ network2.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from tqdm import tqdm
 
 class Net(nn.Module):
     def __init__(self, netShape):
@@ -20,5 +21,43 @@ class Net(nn.Module):
 
         return F.log_softmax(X, dim=1)
 
-    def train(self):
-        pass
+    def train(self, epochNum):
+        count = 0
+        for lr, EPOCHS in zip([0.001],[epochNum]):
+            print("---------- Learning Rate: {} ----------".format(lr))
+            optimizer = optim.Adam(net.parameters(), lr=lr)
+            # EPOCHS = 500
+            for epoch in range(EPOCHS):
+                for X, Y in tqdm(zip(train_batches, train_labels), total=len(train_labels)):
+                    X, Y = X.to(device), Y.to(device)
+                    net.zero_grad()
+                    output = net(X.view(-1,2*imbedDim))
+                    loss = F.nll_loss(output, Y)
+                    loss.backward()
+                    optimizer.step()
+                count += 1
+                correct = 0
+                total = 0
+                with torch.no_grad():
+                    for X, Y in zip(test_batches, test_labels):
+                        X, Y = X.to(device), Y.to(device)
+                        output = net(X.view(-1,2*imbedDim))
+                        for idx, i in enumerate(output):
+                            if torch.argmax(i) == Y[idx]:
+                                correct += 1
+                            total += 1
+                print("Epoch {} / {} | Loss: {} | Accuracy: {}".format(count, epochNum, np.round(torch.Tensor.cpu(loss.detach()).numpy(), 4), round(correct*100 / total,2)))
+
+    def test(self):
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for X, Y in zip(test_batches, test_labels):
+                X, Y = X.to(device), Y.to(device)
+                output = net(X.view(-1,2*imbedDim))
+                for idx, i in enumerate(output):
+                    if torch.argmax(i) == Y[idx]:
+                        correct += 1
+                    total += 1
+            print("Accuracy: ", correct / total)
+        return correct / total
