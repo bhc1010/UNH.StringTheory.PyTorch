eff232f99d3c8c06540349b2554e4787f56d91f0
bhc1010 <61128244+bhc1010@users.noreply.github.com>
2020-03-11 20:39:35 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,18 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import polytope
-import dataLoader
-import numpy as np
+from polytope import Polytope
+from dataLoader import loadData
+from network2 import Net
 
-imbedDim = 25
+imbedDim = 6
 
-
-poly0 = polytope.Polytope([(0.,1.),(-1.,-1.), (1.,0.)], 0, imbedDim)
-poly1 = polytope.Polytope([(-1.,2.),(-1.,-1.),(2.,-1.)], 1, imbedDim)
-poly2 = polytope.Polytope([(0.,1.),(-1.,0.),(-1.,-1.),(1.,0.)], 2, imbedDim)
-poly3 = polytope.Polytope([(-1.,2.),(-1.,-1.),(1.,-1.),(1.,0.)], 3, imbedDim)
-poly4 = polytope.Polytope([(0.,1.),(-1.,1.),(-1.,-1.),(1.,0.)], 4, imbedDim)
-poly5 = polytope.Polytope([(-1.,2.),(-1.,-1.),(-1.,0.),(1.,0.)], 5, imbedDim)
-poly6 = polytope.Polytope([(0.,1.),(-1.,0.),(-1.,-1.),(0.,-1.),(1.,0.)], 6, imbedDim)
-poly7 = polytope.Polytope([(0.,1.),(-1.,1.),(-1.,-1.),(1.,-1.),(1.,0.)], 7, imbedDim)
+poly0 = Polytope([(0.,1.),(-1.,-1.), (1.,0.)], 0, imbedDim)
+poly1 = Polytope([(-1.,2.),(-1.,-1.),(2.,-1.)], 1, imbedDim)
+poly2 = Polytope([(0.,1.),(-1.,0.),(-1.,-1.),(1.,0.)], 2, imbedDim)
+poly3 = Polytope([(-1.,2.),(-1.,-1.),(1.,-1.),(1.,0.)], 3, imbedDim)
+poly4 = Polytope([(0.,1.),(-1.,1.),(-1.,-1.),(1.,0.)], 4, imbedDim)
+poly5 = Polytope([(-1.,2.),(-1.,-1.),(-1.,0.),(1.,0.)], 5, imbedDim)
+poly6 = Polytope([(0.,1.),(-1.,0.),(-1.,-1.),(0.,-1.),(1.,0.)], 6, imbedDim)
+poly7 = Polytope([(0.,1.),(-1.,1.),(-1.,-1.),(1.,-1.),(1.,0.)], 7, imbedDim)
 
 # poly8 = polytope.Polytope([], 0, imbedDim)
 # poly9 = polytope.Polytope([(-1.,2.),(-1.,-1.),(2.,-1.)], 1, imbedDim)
@@ -27,25 +23,20 @@ poly7 = polytope.Polytope([(0.,1.),(-1.,1.),(-1.,-1.),(1.,-1.),(1.,0.)], 7, imbe
 # poly14 = polytope.Polytope([(0.,1.),(-1.,0.),(-1.,-1.),(0.,-1.),(1.,0.)], 6, imbedDim)
 # poly15 = polytope.Polytope([(0.,1.),(-1.,1.),(-1.,-1.),(1.,-1.),(1.,0.)], 7, imbedDim)
 
-polytopes = [poly0, poly1, poly2, poly3]
-
-for poly in polytopes:
-    poly.generateData()
+polytopes = [poly0, poly1, poly2, poly3, poly4, poly5, poly6, poly7]
 
 ## Note batch_size must divide evenly into len(train_data_raw)
-train_batches, train_labels, test_batches, test_labels = dataLoader.loadData(polytopes, batch_size=12)
+train_data, test_data = loadData(polytopes, batch_size=12)
 
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
-    print("Running on the gpu")
-else:
-    device = torch.device("cpu")
-    print("Running on the cpu")
-    
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Running on", device)
+
 # device = torch.device("cpu")
-net = Net([2 * imbedDim, 25,25,25, 8]).to(device)
-net.train(10)
-accuracy = net.test()
+EPOCHS = 100
+
+net = Net([2 * imbedDim, 10, 8], imbedDim, device).to(device)
+net.train(train_data, test_data, EPOCHS)
+accuracy = net.test(test_data)
 
 if accuracy * 100 >= 80:
     torch.save(net.state_dict(),'model4.pt')
